b81961c693a63ecb76c83527e47679a1bd0bb77f
hyunjung <hazelc3837@gmail.com>
2022-06-29 22:07:47 +0900

    modify:ExpensesDate 오류 수정

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,4 +1,4 @@
-import "./ExpenseItem.css";
+import "./ExpenseDate.css";
 
 const ExpenseDate = (props) => {
   const month = props.date.toLocaleString("en-US", { month: "long" });
@@ -7,9 +7,9 @@ const ExpenseDate = (props) => {
 
   return (
     <div className="expense-date">
-      <div className="expense-date_month">{month}</div>
-      <div className="expense-date_year">{year}</div>
-      <div className="expense-date_day">{day}</div>
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day ">{day}</div>
     </div>
   );
 };
