21ea7fcc2035271f4000edce690b8dbefe462a83
hyunjung <hazelc3837@gmail.com>
2022-07-04 22:01:28 +0900

    add: state component 옮기기

--- src/App.js
+++ src/App.js
@@ -26,6 +26,11 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expense);
+  };
   // return React.createElement(
   //   "div",
   //   {},
@@ -37,7 +42,7 @@ function App() {
 
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,35 +1,61 @@
 import React, { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
-  //   const [enteredTitle, setEnteredTitle] = useState("");
-  //   const [enteredAmount, setEnteredAmount] = useState("");
-  //   const [enteredDate, setEnteredDate] = useState("");
-  const [userInput, setUserInput] = useState({
-    enteredTitle: "",
-    enteredAmount: "",
-    enteredDate: "",
-  });
+const ExpenseForm = (props) => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  //   const [userInput, setUserInput] = useState({
+  //     enteredTitle: "",
+  //     enteredAmount: "",
+  //     enteredDate: "",
+  //   });
   const titleChangeHandler = (event) => {
     //컴포넌트 함수의 수명 주기와는 별개인 어떤 변수를 저장하고 있는 것을 확실하게 하기 위해서
-    //setEnteredTitle(event.target.value);
-    setUserInput({ ...userInput, enteredTitle: event.target.value });
+
+    //setUserInput({ ...userInput, enteredTitle: event.target.value });
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
+    setEnteredTitle(event.target.value);
   };
-  const AmountChangeHandler = (event) => {
-    //setEnteredAmount(event.target.value);
-    setUserInput({ ...userInput, enteredAmount: event.target.value });
+  const amountChangeHandler = (event) => {
+    //setEnteredAmount({ ...userInput, enteredTitle: event.target.value });
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredAmount: event.target.value };
+    // });
+    setEnteredAmount(event.target.value);
   };
-  const DateChangeHandler = (event) => {
-    //setEnteredDate(event.target.value);
-    setUserInput({ ...userInput, enteredDate: event.target.value });
+  const dateChangeHandler = (event) => {
+    //setEnteredDate({ ...userInput, enteredTitle: event.target.value });
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredDate: event.target.value };
+    // });
+    setEnteredDate(event.target.value);
+  };
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    props.onSaveExpenseData(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -37,7 +63,8 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
-            onChange={AmountChangeHandler}
+            value={enteredAmount}
+            onChange={amountChangeHandler}
           />
         </div>
         <div className="new-expense__control">
@@ -46,7 +73,8 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             step="2022-12-31"
-            onChange={DateChangeHandler}
+            value={enteredDate}
+            onChange={dateChangeHandler}
           />
         </div>
       </div>
