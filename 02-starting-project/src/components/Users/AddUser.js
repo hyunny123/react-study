99c7147ab67985c88667f0a4feb598e178cf2d0b
hyunjung <hazelc3837@gmail.com>
2022-07-10 14:26:44 +0900

    add: input 조건에 따른 유효성 구현

--- 02-starting-project/src/components/Users/AddUser.js
+++ 02-starting-project/src/components/Users/AddUser.js
@@ -6,10 +6,18 @@ import classes from "./AddUser.module.css";
 const AddUser = (props) => {
   const [enteredUserName, setEnteredUserName] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  //유효한 입력값을 갖는 경우에만 코드 실행.
   const addUserHandler = (event) => {
     event.preventDefault();
+    if (enteredUserName.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if (enteredAge < 1) {
+      return;
+    }
     console.log(enteredUserName, enteredAge);
     //reset : input에 현재 상태를 반영하지 않으면 reset되지 않음
+    //input요소는 속성으로 value를 갖는데 value에 사용자가 입력한 값을 설정할수 있다.value={enteredAge}
     setEnteredUserName("");
     setEnteredAge("");
   };
