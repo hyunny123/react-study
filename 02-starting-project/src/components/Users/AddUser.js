976fa489b8ac96f248e13c976ad9ba00915f6ee4
hyunjung <hazelc3837@gmail.com>
2022-07-10 14:21:35 +0900

    add:input 입력창 초기화 구현

--- 02-starting-project/src/components/Users/AddUser.js
+++ 02-starting-project/src/components/Users/AddUser.js
@@ -9,6 +9,9 @@ const AddUser = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
     console.log(enteredUserName, enteredAge);
+    //reset : input에 현재 상태를 반영하지 않으면 reset되지 않음
+    setEnteredUserName("");
+    setEnteredAge("");
   };
 
   const usernameChangeHandler = (event) => {
@@ -21,9 +24,19 @@ const AddUser = (props) => {
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" onChange={usernameChangeHandler} />
+        <input
+          id="username"
+          type="text"
+          value={enteredUserName}
+          onChange={usernameChangeHandler}
+        />
         <label htmlFor="age">Age(Years)</label>
-        <input id="age" type="number" onChange={ageChangeHandler} />
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
