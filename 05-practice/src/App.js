5fe4323eb18a1425bbfb7b6388455107456146dc
hyunjung <hazelc3837@gmail.com>
2022-08-31 19:47:07 +0900

    add:useMemo

--- 05-practice/src/App.js
+++ 05-practice/src/App.js
@@ -1,12 +1,15 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 
 import Button from "./components/UI/Button/Button";
 import DemoOutput from "./components/Demo/DemoOutput";
+import DemoList from "./components/Demo/DemoList";
 import "./App.css";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
   const [allowToggle, setAllowToggle] = useState(false);
+
+  const [listTitle, setListTitle] = useState("My List");
 
   console.log("APP RUNNING");
 
@@ -21,12 +24,24 @@ function App() {
   const allowToggleHandler = () => {
     setAllowToggle(true);
   };
+
+  const changeTitleHandler = useCallback(() => {
+    setListTitle("New Title");
+  }, []);
+
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
   return (
-    <div className="app">
-      <h1>여러분 안녕하세요!</h1>
-      <DemoOutput show={showParagraph} />
-      <Button onClick={allowToggleHandler}>확인 더보기중</Button>
-      <Button onClick={toggleParagraphHandler}>더보기</Button>
+    <div>
+      <div className="app">
+        <h1>여러분 안녕하세요!</h1>
+        <DemoOutput show={showParagraph} />
+        <Button onClick={allowToggleHandler}>확인 더보기중</Button>
+        <Button onClick={toggleParagraphHandler}>더보기</Button>
+      </div>
+      <div className="app">
+        <DemoList title={listTitle} items={listItems} />
+        <Button onClick={changeTitleHandler}>제목 바꾸기</Button>
+      </div>
     </div>
   );
 }
